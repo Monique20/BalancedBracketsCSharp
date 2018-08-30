a89a9583cf02e8c6f662776479442083daacf356
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-08-30 12:06:44 +0200

    added kata in c#

---
+++ BalancedBracketsKata/BalancedBracketsKata/BalancedBrackets.cs
@@ -0,0 +1,35 @@
+﻿namespace BalancedBracketsKata
+{
+    public class BalancedBrackets
+    {
+        public string ValidateBrackets(string input)
+        {
+            if (NullOrWhiteSpace(input))
+            {
+                return "OK";
+            }
+            var counter = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '[')
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter--;
+                }
+                if(counter < 0)
+                {
+                    return "FAIL";
+                }
+            }
+            return "OK";
+        }
+
+        private bool NullOrWhiteSpace(string input)
+        {
+            return string.IsNullOrWhiteSpace(input);
+        }
+    }
+}
---
+++ BalancedBracketsKata/BalancedBracketsKataTests/BalancedBracketsTests.cs
@@ -0,0 +1,55 @@
+﻿using BalancedBracketsKata;
+using NUnit.Framework;
+
+namespace BalancedBracketsKataTests
+{
+    [TestFixture]
+    public class BalancedBracketsTests
+    {
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase(null)]
+        public void ValidateBrackets_GivenInputWithEmptyOrNullOrWhiteSpace_ShouldReturnOK(string input)
+        {
+            //Arrange
+            var sut = new BalancedBrackets();
+
+            //Act
+            var actual = sut.ValidateBrackets(input);
+
+            //Assert
+            var expected = "OK";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("][","FAIL")]
+        [TestCase("][[","FAIL")]
+        [TestCase("][]", "FAIL")]
+        public void ValidateBrackets_GivenSetOfUnbalancedBrackets_ShouldReturnFAIL(string input, string expected)
+        {
+            //Arrange
+            var sut = new BalancedBrackets();
+
+            //Act
+            var actual = sut.ValidateBrackets(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("[]", "OK")]
+        [TestCase("[[][]]", "OK")]
+        [TestCase("[[]]", "OK")]
+        public void ValidateBrackets_GivenSetOfBalancedBrackets_ShouldReturnOK(string input, string expected)
+        {
+            //Arrange
+            var sut = new BalancedBrackets();
+
+            //Act
+            var actual = sut.ValidateBrackets(input);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
